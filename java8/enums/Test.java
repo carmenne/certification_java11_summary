a910fdcbdad4fca2d78f621ba3aeb2804c148fd9
Carmen <carmen.pomana04@gmail.com>
2020-02-14 21:32:45 +0100

    Added some enum tests

---
+++ java8/enums/Test.java
@@ -0,0 +1,40 @@
+enum Micutenii {
+	
+	MICUTA("Carmen", 32) { // constant specific class body
+		@Override
+		public String toString() {
+			return "MICUTICA!!!";
+		}
+	},
+	MICUTUL("Teodor", 34);
+	
+	public String name;
+	public int age;
+	
+	private Micutenii(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+	@Override
+	public String toString() {
+		return name + " " + age;
+	}
+	
+	// error: method values() is already defined in enum Micutenii
+	//~ public Micutenii[] values() {
+		//~ return new Micutenii[]{};
+	//~ }
+	
+}
+
+public class Test {
+	
+	public static void main(String[] args) {
+		
+		System.out.println(Micutenii.MICUTA + " " + Micutenii.MICUTUL);
+		Micutenii.MICUTA.name = "tot Carmen";
+		System.out.println(Micutenii.MICUTA + " " + Micutenii.MICUTUL);
+		//~ System.out.println(new Micutenii("a treia micutenie?", 0)); // error: enum types may not be instantiated
+	}
+}
